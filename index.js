916d557460e0845be1f6083e24de5bd0dfbeeda1
Tesfa Tesfaye <tesfaget15@gmail.com>
2022-11-23 15:37:41 -0500

    new commit

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const server=http.createServer((req,res)=>{
             case '.js':
                 contentType='text/javascript'
                  break
-            case '.css':
+           case '.css':
                 contentType='text/css'
                 break
             case '.json':
@@ -55,16 +55,21 @@ const server=http.createServer((req,res)=>{
 
 
         }
+        console.log(path.extname(filePath))
  
         //Read File
         fs.readFile(filePath, (err,content)=>{
             if(err){
                 if(err.code == "ENOENT"){
-                    // Page not found
-                    fs.readFile(path.join(__dirname, 'public', '404.html'),(err,content)=>{
-                        res.writeHead(200, {'Content-Type': 'text/html'})
-                        res.end(content,'utf-8')
+                   
+                    fs.writeFile(path.join(__dirname,'public', req.url),`<h1>${(req.url).slice(1)}</h1>`,err=>{
+                        fs.readFile(path.join(__dirname, 'public', req.url),(err,content)=>{
+                            res.writeHead(200, {'Content-Type': {contentType}})
+                            res.end(content,'utf-8')
+                        })
                     })
+                    // Page not found
+                   
                     
                 }
             else{
