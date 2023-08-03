c781b08d8f55f7c552a7cd8fcd19fff6b2a9ca78
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-08-03 17:21:32 -0400

    made modifications to the typeis function to make it more optimal

--- typeIs.js
+++ typeIs.js
@@ -3,19 +3,8 @@ const typeIs = (input) => {
   if (typeof input !== "object") {
     return typeof input;
   }
-  let instanceHolder = new Map([
-    ["array", Array],
-    ["map", Map],
-    ["set", Set],
-    ["regexp", RegExp],
-    ["weakmap", WeakMap],
-    [["weakset", WeakSet]],
-    ["date", Date],["error",Error]
-  ]);
-  for (let i of instanceHolder) {
-    if (input instanceof i[1]) return i[0];
-  }
-  return (typeof input==="object" ? "object" : new Error("Can't find type"))
+  let type = (input?.constructor?.name).toLowerCase();
+  return type ?? new Error("unknownType");
 };
 
 export default typeIs
